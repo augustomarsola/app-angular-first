d2ce17a800f300f3becc266a490536be67148a99
augustomarsola <gugumarsola@hotmail.com>
2021-08-23 19:47:13 -0300

    Definindo os componentes de navegação

---
+++ src/app/navegacao/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+
+//Aqui removemos o OnInit pois não iremos utilizar
+export class HomeComponent {}
---
+++ src/app/navegacao/menu/menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html'
+})
+
+//Aqui removemos o OnInit pois não iremos utilizar
+export class MenuComponent {}
